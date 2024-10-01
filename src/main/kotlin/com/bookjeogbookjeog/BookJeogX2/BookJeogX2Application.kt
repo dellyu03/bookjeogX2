22768f47be096681077047926c94dc7abfc9b7e5
dellyu03 <seunghwan081@gmail.com>
2024-10-01 18:04:17 +0900

    init project

---
+++ src/main/kotlin/com/bookjeogbookjeog/BookJeogX2/BookJeogX2Application.kt
@@ -0,0 +1,11 @@
+package com.bookjeogbookjeog.BookJeogX2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookJeogX2Application
+
+fun main(args: Array<String>) {
+	runApplication<BookJeogX2Application>(*args)
+}
